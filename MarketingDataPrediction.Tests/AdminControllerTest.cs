92e080e26cd774d01912283d81d5dd98bf114f72
Marek Sagan <marek.sagan@protonmail.com>
2018-05-08 19:23:57 +0200

    Included NSubstitute and xunit.NET

--- MarketingDataPrediction.Tests/AdminControllerTest.cs
+++ MarketingDataPrediction.Tests/AdminControllerTest.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace MarketingDataPrediction.Tests
 {
-    class AdminControllerTest
+    public class AdminControllerTest
     {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
